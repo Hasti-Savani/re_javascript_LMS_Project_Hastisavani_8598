1dd9477c9bc00a183d47df83b94f7cc451deb0a6
Hasti Savani <hastisavani@192.168.1.10>
2025-08-28 20:15:03 +0530

    Project

---
+++ index.js
@@ -0,0 +1,14 @@
+// Basic Calculation
+
+{
+    let a = 10
+    let b = 20
+    console.log("Addition:", a + b);
+    console.log("Subtraction:", a - b);
+    console.log("Multiplication:", a * b);
+    console.log("Division:", a / b);
+    console.log("Modulus:", a % b);
+    console.log("Exponentiation:", a ** b);
+    console.log("increment:", ++a, b++);
+    console.log("decrement:", --a, b--);
+}
